c0e90816377e4787e0ef32888280ee7eea81c426
Fede <fedelva8@gmail.com>
2022-09-29 23:48:41 -0300

    creo migracion califications_table (tabla de calficaciones)

---
+++ database/migrations/2022_09_30_021226_create_califications_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('califications', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('subject_id')
+                ->constrained('subjects')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->tinyInteger('annual');
+
+            $table->tinyInteger('december');
+
+            $table->tinyInteger('february');  
+
+            $table->tinyInteger('due_subject');
+
+            $table->date('due_subject_date');  
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('califications');
+    }
+};
