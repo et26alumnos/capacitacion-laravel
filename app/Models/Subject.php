c176a061c2117ef1e34d5154994f74a43692a6ae
Francisco Trujillo <trujilloszitai@gmail.com>
2022-10-10 18:34:10 -0300

    Modelo Subject; doctrine/dbal agregado a dependencias

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -4,11 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Subject extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
+    protected $table = 'subjects';
+    protected $primaryKey = 'id';
     protected $fillable = [
         'name'
     ];
