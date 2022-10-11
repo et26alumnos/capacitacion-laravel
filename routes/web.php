cb7db675679163dd781ce42872c5a6b253187c36
Francisco Trujillo <trujilloszitai@gmail.com>
2022-10-11 01:48:18 -0300

    Rutas de materias

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\StudentController;
+use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,20 @@ use App\Http\Controllers\StudentController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Rutas de materias
+
+Route::get('/subject/{load?}', [SubjectController::class, 'index']);
+
+Route::get('/subject/{id}', [SubjectController::class, 'show'])->where('id', '[0-9]+');
+
+Route::get('/subject/new', [SubjectController::class, 'create']);
+
+Route::post('/subject', [SubjectController::class, 'store']);
+
+Route::get('/subject/{id}/edit', [SubjectController::class, 'edit'])->where('id', '[0-9]+');
+
+Route::put('/subject/{subject}', [SubjectController::class, 'update']);
+
+Route::delete('/subject/{id}', [SubjectController::class, 'destroy'])->where('id', '[0-9]+');
